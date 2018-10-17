3f2a0b14a225fd604c19e638af4c77e0cc17b1a7
Deepakumar <gdeepakumar@gmail.com>
2018-10-17 15:22:02 +0300

    hiden

--- Fundermental/src/app/events/event-thumbnail.component.ts
+++ Fundermental/src/app/events/event-thumbnail.component.ts
@@ -9,13 +9,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         <div>Date: {{event.date}}</div>
         <div>Time: {{event.time}}</div>
         <div>Price: {{event.price}}</div>
+        <div *ngIf="event?.location">
+            <span>{{event.location.address}}</span>
+            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online URL: {{event.onlineUrl}}
+        </div>
         <button class="btn btn-primary">Clicked me!</button>
         </div>
         </div>
     `,
     styles : [
         `.pad-left { margin-left: 10px; }
-        .well div { color: #bbb;}
+        .well div { color: #bbb; font-size:0.75em;}
+        .thumbnail { min-height: 210px;}
         `
     ]
 })
