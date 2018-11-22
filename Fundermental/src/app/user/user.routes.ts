67a449a13b0ec1919a708a5b518b809091f99ec2
Deepakumar <gdeepakumar@gmail.com>
2018-11-22 07:20:11 +0300

    user profile

---
+++ Fundermental/src/app/user/user.routes.ts
@@ -0,0 +1,6 @@
+import { ProfileComponent} from './profile.component'
+import { Profile } from 'selenium-webdriver/firefox';
+
+export const userRoutes = [
+    {path:'profile',component: ProfileComponent}
+]
