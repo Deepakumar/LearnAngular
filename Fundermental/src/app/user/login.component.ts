1012437bd9c9acaef09829b8a6c37080191723ae
Deepakumar <gdeepakumar@gmail.com>
2018-11-23 09:33:37 +0300

    03.Using-the-Data-from-Your-Template-based-Form

--- Fundermental/src/app/user/login.component.ts
+++ Fundermental/src/app/user/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "./auth.service";
+import { Router} from "@angular/router"
 
 @Component({
     templateUrl:'./login.component.html'
@@ -9,10 +10,14 @@ export class LoginComponent {
     username:string
     password:string
 
-    constructor(private authService:AuthService) {
+    constructor(private authService:AuthService,private router:Router) {
 
     }
     login(formValues) {
         this.authService.loginUser(formValues.username,formValues.password);
+        this.router.navigate(['events'])
+    }
+    cancel(){
+        this.router.navigate(['events'])
     }
 }
